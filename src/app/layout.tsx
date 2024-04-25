290f352bc4ad1ca23ba615b12ae29f27351c754c
José Vielma <josevglod@gmail.com>
2024-04-25 11:30:20 -0400

    add provider to entire application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import ReduxProvider from "@/store/reduxProvider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,8 +15,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="es">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ReduxProvider>
+      <html lang="es">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </ReduxProvider>
   );
 }
