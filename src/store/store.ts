468cf5ee82e2f00c4b37e57e5bf17b27c5f254fd
José Vielma <josevglod@gmail.com>
2024-05-01 00:28:37 -0400

    fix persist store values

--- src/store/store.ts
+++ src/store/store.ts
@@ -7,7 +7,7 @@ import storage from "redux-persist/lib/storage";
 const authPersistConfig = {
   key: "auth",
   storage: storage,
-  whitelist: ["authState"],
+  whitelist: ["authState", "token", "role"],
 };
 
 const rootReducer = combineReducers({
