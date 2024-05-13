b0c4e7f638ea179c4644f98b83cd4a36dd039450
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-05-12 22:37:21 -0400

    fixed styles in components - discharge

--- src/app/discharge/page.tsx
+++ src/app/discharge/page.tsx
@@ -1,35 +1,59 @@
-"use client"
-import React, { useState } from 'react'
-import "@/styles/discharge/discharge.css";
-import { Box, Heading, Image, Radio, RadioGroup, Text } from '@chakra-ui/react';
-import Link from 'next/link';
+"use client";
+import React, { useState } from "react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Image,
+  Radio,
+  RadioGroup,
+  Text,
+  Button,
+} from "@chakra-ui/react";
+import Link from "next/link";
 
 function Discharge() {
-  const [answer1, setAnswer1] = useState<string>('');
-    const [answer2, setAnswer2] = useState<string>('');
-    const [answer3, setAnswer3] = useState<string>('');
-    const [answer4, setAnswer4] = useState<string>('');
-    const [answer5, setAnswer5] = useState<string>('');
+  const [answer1, setAnswer1] = useState<string>("");
+  const [answer2, setAnswer2] = useState<string>("");
+  const [answer3, setAnswer3] = useState<string>("");
+  const [answer4, setAnswer4] = useState<string>("");
+  const [answer5, setAnswer5] = useState<string>("");
 
-    const allAnswersProvided = answer1 && answer2 && answer3 && answer4 && answer5;
+  const allAnswersProvided =
+    answer1 && answer2 && answer3 && answer4 && answer5;
 
-    const handleSubmit = () => {
-      const answers = {
-          answer1: answer1,
-          answer2: answer2,
-          answer3: answer3,
-          answer4: answer4,
-          answer5: answer5,
-      }
-      console.log(answers)
-  }
+  const handleSubmit = () => {
+    const answers = {
+      answer1: answer1,
+      answer2: answer2,
+      answer3: answer3,
+      answer4: answer4,
+      answer5: answer5,
+    };
+    console.log(answers);
+  };
 
   return (
     <>
-    <Box className="CaresHeader">
-        <Box className="circle">
-          <Box className="returnButton">
-          <Image
+      <Flex w="100vw" h="13vh" justify="space-between" pr="3vh">
+        <Box
+          w="18vh"
+          h="18vh"
+          bg="#AD8EB1"
+          pt="2vh"
+          pl="2vh"
+          sx={{ clipPath: "circle(66.4% at 1% 1%)" }}
+        >
+          <Box
+            w="5vh"
+            h="5vh"
+            borderRadius="35px"
+            bg="white"
+            p="5px"
+            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+            justifyContent="center"
+          >
+            <Image
               src="/arrow.png"
               alt="arrow"
               width={10}
@@ -38,137 +62,227 @@ function Discharge() {
             />
           </Box>
         </Box>
-        <Box className="headerContent">
-          <Heading as="h1" className="perfilTitle">
+        <Flex w="65vw" direction="column" align="flex-end" justify="center">
+          <Heading
+            fontWeight="bold"
+            color="#4F1964"
+            fontSize="30px"
+            mt="6vh"
+            mb="1vh"
+            mr="10px"
+          >
             Alta del paciente
           </Heading>
-          <Box className="divider" />
-        </Box>
-      </Box>
-      <Box className="survey">
-      <Box className="surveyContent">
-          <Image src="/discharge/nurse.png" alt="nurse" width={105} height={105} />
-          <Box display="flex" flexDirection="column" justifyContent="center" marginLeft={5}>
-          <Text className='surveyText'>
+          <Box w="55vw" h="2px" bg="#AD8EB1" />
+        </Flex>
+      </Flex>
+      <Flex
+        w="100vw"
+        pl="20px"
+        pr="20px"
+        mb="30px"
+        direction="column"
+        align="center"
+      >
+        <Flex
+          w="100%"
+          mb="20px"
+          mt="20px"
+          direction="row"
+          justify="space-between"
+        >
+          <Image
+            src="/discharge/nurse.png"
+            alt="nurse"
+            width={105}
+            height={105}
+          />
+          <Box
+            display="flex"
+            flexDirection="column"
+            justifyContent="center"
+            marginLeft={5}
+          >
+            <Text fontWeight="bold" color="#3B3B3B" fontSize="16px">
               Estimado/a paciente:
             </Text>
-            <Text fontSize={14}>Le pedimos de manera más comedida que responda la siguiente encuesta de satisfacción, sobre esta herramienta que utilizó durante el proceso de cicatrización de su herida. </Text>
+            <Text fontSize={14}>
+              Le pedimos de manera más comedida que responda la siguiente
+              encuesta de satisfacción, sobre esta herramienta que utilizó
+              durante el proceso de cicatrización de su herida.{" "}
+            </Text>
           </Box>
-          </Box>
-          <Text color={"#4F1964"} fontWeight={500}>Por favor, deberá seleccionar la opción que describa su experiencia con el uso de esta aplicación.</Text>
-          <Box className="surveyContent">
+        </Flex>
+        <Text color={"#4F1964"} fontWeight={500}>
+          Por favor, deberá seleccionar la opción que describa su experiencia
+          con el uso de esta aplicación.
+        </Text>
+        <Flex
+          w="100%"
+          mb="20px"
+          mt="20px"
+          direction="row"
+          justify="space-between"
+        >
           <Box display="flex" flexDirection="column" justifyContent="center">
-          <Text className='surveyText2'>
-          La herramienta me ayudó en el proceso de cicatrización de mi herida:
+            <Text fontWeight="600" color="#3B3B3B" fontSize="16px">
+              La herramienta me ayudó en el proceso de cicatrización de mi
+              herida:
             </Text>
-            <RadioGroup display="flex" flexDirection="column" marginTop={3} onChange={setAnswer1}>
-              <Radio value="Muy satisfecho" size="lg" colorScheme='purple'>
+            <RadioGroup
+              display="flex"
+              flexDirection="column"
+              marginTop={3}
+              onChange={setAnswer1}
+            >
+              <Radio value="Muy satisfecho" size="lg" colorScheme="purple">
                 Muy satisfecho
               </Radio>
-              <Radio value="Satisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Satisfecho" size="lg" colorScheme="purple">
                 Satisfecho
               </Radio>
-              <Radio value="Neutral" size="lg" colorScheme='purple'>
+              <Radio value="Neutral" size="lg" colorScheme="purple">
                 Neutral
               </Radio>
-              <Radio value="Insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Insatisfecho" size="lg" colorScheme="purple">
                 Insatisfecho
               </Radio>
-              <Radio value="Muy insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Muy insatisfecho" size="lg" colorScheme="purple">
                 Muy insatisfecho
               </Radio>
             </RadioGroup>
-            <Box className="surveyDivider" />
-            <Text className='surveyText2'>
-          La herramienta es fácil de usar:
+            <Box w="100%" h="2px" bg="#AD8EB1" mt="15px" mb="15px" />
+            <Text fontWeight="600" color="#3B3B3B" fontSize="16px">
+              La herramienta es fácil de usar:
             </Text>
-            <RadioGroup display="flex" flexDirection="column" marginTop={3} onChange={setAnswer2}>
-              <Radio value="Muy satisfecho" size="lg" colorScheme='purple'>
+            <RadioGroup
+              display="flex"
+              flexDirection="column"
+              marginTop={3}
+              onChange={setAnswer2}
+            >
+              <Radio value="Muy satisfecho" size="lg" colorScheme="purple">
                 Muy satisfecho
               </Radio>
-              <Radio value="Satisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Satisfecho" size="lg" colorScheme="purple">
                 Satisfecho
               </Radio>
-              <Radio value="Neutral" size="lg" colorScheme='purple'>
+              <Radio value="Neutral" size="lg" colorScheme="purple">
                 Neutral
               </Radio>
-              <Radio value="Insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Insatisfecho" size="lg" colorScheme="purple">
                 Insatisfecho
               </Radio>
-              <Radio value="Muy insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Muy insatisfecho" size="lg" colorScheme="purple">
                 Muy insatisfecho
               </Radio>
             </RadioGroup>
-            <Box className="surveyDivider" />
-            <Text className='surveyText2'>
-            La herramienta tiene recursos que me ayudaron a responder mis dudas:
+            <Box w="100%" h="2px" bg="#AD8EB1" mt="15px" mb="15px" />
+            <Text fontWeight="600" color="#3B3B3B" fontSize="16px">
+              La herramienta tiene recursos que me ayudaron a responder mis
+              dudas:
             </Text>
-            <RadioGroup display="flex" flexDirection="column" marginTop={3} onChange={setAnswer3}>
-              <Radio value="Muy satisfecho" size="lg" colorScheme='purple'>
+            <RadioGroup
+              display="flex"
+              flexDirection="column"
+              marginTop={3}
+              onChange={setAnswer3}
+            >
+              <Radio value="Muy satisfecho" size="lg" colorScheme="purple">
                 Muy satisfecho
               </Radio>
-              <Radio value="Satisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Satisfecho" size="lg" colorScheme="purple">
                 Satisfecho
               </Radio>
-              <Radio value="Neutral" size="lg" colorScheme='purple'>
+              <Radio value="Neutral" size="lg" colorScheme="purple">
                 Neutral
               </Radio>
-              <Radio value="Insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Insatisfecho" size="lg" colorScheme="purple">
                 Insatisfecho
               </Radio>
-              <Radio value="Muy insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Muy insatisfecho" size="lg" colorScheme="purple">
                 Muy insatisfecho
               </Radio>
             </RadioGroup>
-            <Box className="surveyDivider" />
-            <Text className='surveyText2'>
-            Necesito ayuda de personal experto para utilizar esta aplicación:
+            <Box w="100%" h="2px" bg="#AD8EB1" mt="15px" mb="15px" />
+            <Text fontWeight="600" color="#3B3B3B" fontSize="16px">
+              Necesito ayuda de personal experto para utilizar esta aplicación:
             </Text>
-            <RadioGroup display="flex" flexDirection="column" marginTop={3} onChange={setAnswer4}>
-              <Radio value="Muy satisfecho" size="lg" colorScheme='purple'>
+            <RadioGroup
+              display="flex"
+              flexDirection="column"
+              marginTop={3}
+              onChange={setAnswer4}
+            >
+              <Radio value="Muy satisfecho" size="lg" colorScheme="purple">
                 Muy satisfecho
               </Radio>
-              <Radio value="Satisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Satisfecho" size="lg" colorScheme="purple">
                 Satisfecho
               </Radio>
-              <Radio value="Neutral" size="lg" colorScheme='purple'>
+              <Radio value="Neutral" size="lg" colorScheme="purple">
                 Neutral
               </Radio>
-              <Radio value="Insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Insatisfecho" size="lg" colorScheme="purple">
                 Insatisfecho
               </Radio>
-              <Radio value="Muy insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Muy insatisfecho" size="lg" colorScheme="purple">
                 Muy insatisfecho
               </Radio>
             </RadioGroup>
-            <Box className="surveyDivider" />
-            <Text className='surveyText2'>
-            Volvería a utilizar esta herramienta:
+            <Box w="100%" h="2px" bg="#AD8EB1" mt="15px" mb="15px" />
+            <Text fontWeight="600" color="#3B3B3B" fontSize="16px">
+              Volvería a utilizar esta herramienta:
             </Text>
-            <RadioGroup display="flex" flexDirection="column" marginTop={3} onChange={setAnswer5}>
-              <Radio value="Muy satisfecho" size="lg" colorScheme='purple'>
+            <RadioGroup
+              display="flex"
+              flexDirection="column"
+              marginTop={3}
+              onChange={setAnswer5}
+            >
+              <Radio value="Muy satisfecho" size="lg" colorScheme="purple">
                 Muy satisfecho
               </Radio>
-              <Radio value="Satisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Satisfecho" size="lg" colorScheme="purple">
                 Satisfecho
               </Radio>
-              <Radio value="Neutral" size="lg" colorScheme='purple'>
+              <Radio value="Neutral" size="lg" colorScheme="purple">
                 Neutral
               </Radio>
-              <Radio value="Insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Insatisfecho" size="lg" colorScheme="purple">
                 Insatisfecho
               </Radio>
-              <Radio value="Muy insatisfecho" size="lg" colorScheme='purple'>
+              <Radio value="Muy insatisfecho" size="lg" colorScheme="purple">
                 Muy insatisfecho
               </Radio>
             </RadioGroup>
           </Box>
-        </Box>
+        </Flex>
         <Link href="/dischargeCongrats">
-        <button className='sendButton' disabled={!allAnswersProvided} onClick={handleSubmit}>Enviar</button>
+          <Button
+            w="80vw"
+            h="6vh"
+            bg="#4F1964"
+            borderRadius="15px"
+            mt="20px"
+            color="white"
+            fontSize="24px"
+            fontWeight="bold"
+            boxShadow="0px 4px 4px rgba(0, 0, 0, 0.25)"
+            _disabled={{
+              bg: "#cccccc",
+              color: "#666666",
+              cursor: "not-allowed",
+            }}
+            isDisabled={!allAnswersProvided}
+            onClick={handleSubmit}
+          >
+            Enviar
+          </Button>
         </Link>
-        </Box></>
-  )
+      </Flex>
+    </>
+  );
 }
 
-export default Discharge
+export default Discharge;
