3c6029ea7291e5b81371a18bab571149ce5cfd66
José Vielma <josevglod@gmail.com>
2024-05-12 21:39:00 -0400

    change login to general fetchApi

--- src/services/auth/login.service.ts
+++ src/services/auth/login.service.ts
@@ -1,25 +1,10 @@
 import { LoginResponse } from "@/interfaces/auth/login.interface";
-import { BASE_URL } from "@/utils/variables";
+import { fetchAPI } from "@/utils/api";
 
 export const login = async (
   nationalId: string,
   password: string
 ): Promise<LoginResponse> => {
-  const response = await fetch(`${BASE_URL}/auth/login`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ nationalId, password }),
-  });
-
-  if (!response.ok) {
-    throw {
-      status: response.status,
-    };
-  }
-
-  const data = await response.json();
-
-  return data;
+  const data = await fetchAPI("/auth/login", "POST", { nationalId, password });
+  return data as LoginResponse;
 };
