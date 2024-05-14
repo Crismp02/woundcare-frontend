55ac660216f0d0ab4868bfb763c106685e38161e
José Vielma <josevglod@gmail.com>
2024-05-14 11:43:33 -0400

    add notification titles to hooks

--- src/hooks/notification.ts
+++ src/hooks/notification.ts
@@ -20,3 +20,13 @@ notificationImages.set(
 notificationImages.set("DISCHARGE", "/notifications/discharge.svg");
 notificationImages.set("MEDICATION_TIME", "/notifications/medication.svg");
 notificationImages.set("BANDAGE_CHANGE", "/notifications/first-aid-kit.svg");
+
+export const notificationTitles = new Map();
+notificationTitles.set("POST_BANDAGE_CHANGE", "Post cambio de vendaje");
+notificationTitles.set(
+  "MONITORING_SIGNS_AND_SYMPTOMS",
+  "Seguimiento de signos y síntomas"
+);
+notificationTitles.set("DISCHARGE", "Alta del paciente");
+notificationTitles.set("MEDICATION_TIME", "Es hora de tu medicación");
+notificationTitles.set("BANDAGE_CHANGE", "Cambio de vendaje");
