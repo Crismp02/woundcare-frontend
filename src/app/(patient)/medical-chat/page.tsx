41a75ddeb54cb9841fa8a7c21a37c9c08a0d8df2
José Vielma <josevglod@gmail.com>
2024-05-26 16:24:21 -0400

    add fetch message, conversation and pagination messages

--- src/app/(patient)/medical-chat/page.tsx
+++ src/app/(patient)/medical-chat/page.tsx
@@ -1,19 +1,87 @@
 "use client";
 import Arrow from "@/components/Arrow";
+import Loader from "@/components/Loader";
 import Chat from "@/components/medical-chat/Chat";
+import PaginationLoader from "@/components/PaginationLoader";
+import { Conversation } from "@/interfaces/chat/conversation.interface";
+import { Message } from "@/interfaces/chat/messages.interface";
+import { GenreEnum, RoleEnum } from "@/interfaces/common/role.type";
+import { getMyConversation } from "@/services/patient/conversation.service";
+import { getMessages } from "@/services/patient/messages.service";
 import { Box, Flex, Heading, Input } from "@chakra-ui/react";
 import Image from "next/image";
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { toast } from "react-toastify";
 
 function MedicalChat() {
   const [message, setMessage] = useState("");
+  const [loading, setLoading] = useState(true);
+  const { ref, inView } = useInView();
+  const [page, setPage] = useState(1);
+  const [totalMessages, setTotalMessages] = useState(0);
+  const [conversation, setConversation] = useState<Conversation>(
+    {} as Conversation
+  );
+  const [messages, setMessages] = useState<Message[]>([]);
+  const chatBoxRef = useRef<HTMLDivElement>(null);
 
-  return (
+  const fetchConversation = async () => {
+    try {
+      const conversation = await getMyConversation();
+      setConversation(conversation);
+    } catch (error: any) {
+      if (error.status === 404) {
+        toast.error("Usted no posee una conversación activa");
+      } else {
+        toast.error("Ha ocurrido un error al cargar la conversación");
+      }
+    }
+  };
+
+  const fetchMessages = async () => {
+    try {
+      const newMessages = await getMessages(page, 10);
+      setMessages([...messages, ...newMessages.items]);
+      if (page === 1) setTotalMessages(newMessages.meta.totalItems);
+      setPage(page + 1);
+    } catch (error) {
+      toast.error("Ha ocurrido un error al cargar los mensajes");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchConversation();
+  }, []);
+
+  useEffect(() => {
+    if (inView || page === 1) fetchMessages();
+  }, [inView]);
+
+  useEffect(() => {
+    const chatBox = chatBoxRef.current;
+    if (chatBox) {
+      const isAtBottom =
+        chatBox.scrollHeight - chatBox.scrollTop === chatBox.clientHeight;
+      if (isAtBottom) {
+        chatBox.scrollTop = chatBox.scrollHeight;
+      }
+    }
+  }, [messages]);
+
+  return loading ? (
+    <Box width={"100vw"} flexGrow={1} position={"relative"}>
+      <Loader />
+    </Box>
+  ) : (
     <Box
       as="main"
       display={"flex"}
       flexDirection={"column"}
       paddingBottom={6}
+      maxHeight={"100vh"}
       flexGrow={1}
     >
       <Arrow />
@@ -34,8 +102,20 @@ function MedicalChat() {
             Mensajes
           </Heading>
         </Flex>
-        <Box flexGrow={1} paddingY={4}>
-          <Chat />
+        <Box
+          ref={chatBoxRef}
+          flexGrow={1}
+          maxHeight={"70vh"}
+          paddingY={4}
+          marginY={4}
+          overflowY={"scroll"}
+        >
+          {!(totalMessages === messages.length) && (
+            <Box ref={ref} display={"flex"} justifyContent={"center"}>
+              <PaginationLoader />
+            </Box>
+          )}
+          <Chat conversation={conversation} messages={messages} />
         </Box>
         <Flex gap={4}>
           <Input
