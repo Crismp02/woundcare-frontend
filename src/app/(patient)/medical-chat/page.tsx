e66c2ce82affa75869ee604902946d6ffa6b38f1
José Vielma <josevglod@gmail.com>
2024-05-26 21:33:02 -0400

    add sockets

--- src/app/(patient)/medical-chat/page.tsx
+++ src/app/(patient)/medical-chat/page.tsx
@@ -8,6 +8,7 @@ import { Message } from "@/interfaces/chat/messages.interface";
 import { GenreEnum, RoleEnum } from "@/interfaces/common/role.type";
 import { getMyConversation } from "@/services/patient/conversation.service";
 import { getMessages } from "@/services/patient/messages.service";
+import { socket } from "@/socket";
 import { Box, Flex, Heading, Input } from "@chakra-ui/react";
 import Image from "next/image";
 import React, { useEffect, useRef, useState } from "react";
@@ -20,11 +21,60 @@ function MedicalChat() {
   const { ref, inView } = useInView();
   const [page, setPage] = useState(1);
   const [totalMessages, setTotalMessages] = useState(0);
-  const [conversation, setConversation] = useState<Conversation>(
-    {} as Conversation
-  );
-  const [messages, setMessages] = useState<Message[]>([]);
+  const [conversation, setConversation] = useState<Conversation>({
+    id: 1,
+    userId: "28534711",
+    user: {
+      fullname: "John Doe",
+      role: RoleEnum.PATIENT,
+      patient: [
+        {
+          genre: GenreEnum.MALE,
+        },
+      ],
+      doctor: [],
+    },
+    nurseId: "nurse456",
+    nurse: {
+      genre: GenreEnum.FEMALE,
+      user: {
+        fullname: "Jane Smith",
+      },
+    },
+    medicalFileId: 789,
+  } as Conversation);
+  const [messages, setMessages] = useState<Message[]>([
+    {
+      id: 1,
+      conversationId: conversation.id,
+      userId: conversation.userId,
+      text: "Hello, how are you?",
+      image: null,
+      createdAt: new Date(),
+      owner: true,
+    },
+    {
+      id: 2,
+      conversationId: conversation.id,
+      userId: conversation.nurseId,
+      text: "I am fine, thank you!",
+      image: null,
+      createdAt: new Date(),
+      owner: false,
+    },
+    {
+      id: 3,
+      conversationId: conversation.id,
+      userId: conversation.userId,
+      text: "AYUDAAAA",
+      image: null,
+      createdAt: new Date(),
+      owner: true,
+    },
+  ]);
   const chatBoxRef = useRef<HTMLDivElement>(null);
+  const [isConnected, setIsConnected] = useState(false);
+  const [transport, setTransport] = useState("N/A");
 
   const fetchConversation = async () => {
     try {
@@ -52,8 +102,62 @@ function MedicalChat() {
     }
   };
 
+  const addMessage = (data: any) => {
+    console.log(data)
+    setMessages((prevMessages) => {
+      if (prevMessages.some((message) => message.id === data.incremental))
+        return prevMessages;
+      return [
+        ...prevMessages,
+        {
+          text: data.message.text,
+          conversationId: 1,
+          createdAt: new Date(),
+          id: data.incremental,
+          owner: true,
+          userId: "28534711",
+          image: null,
+        },
+      ];
+    });
+    goDown()
+  };
+
+  const sendMessage = () => {
+    socket.emit("send-message", {
+      text: message,
+      conversationId: conversation.id,
+    });
+    setMessage("");
+  };
+
   useEffect(() => {
     fetchConversation();
+    socket.on("connect", () => {
+      console.log("Conectado");
+    });
+    socket.on("disconnect", () => {
+      console.log("Desconectado");
+    });
+    socket.on("on-message", (data) => {
+      console.log(data);
+      console.log(messages);
+      addMessage(data);
+    });
+    /*
+    socket.on("messages", (message: Message) => {
+      setMessages((prevMessages) => [...prevMessages, message]);
+    })*/
+
+    return () => {
+      socket.off("connect", () => {
+        console.log("Cleaning");
+      });
+      socket.off("disconnect", () => {
+        console.log("Cleaning");
+      });
+      //socket.off("messages")
+    };
   }, []);
 
   useEffect(() => {
@@ -70,6 +174,17 @@ function MedicalChat() {
       }
     }
   }, [messages]);
+
+  const goDown = () => {
+    const chatBox = chatBoxRef.current;
+    if (chatBox) {
+      const isAtBottom =
+        chatBox.scrollHeight - chatBox.scrollTop === chatBox.clientHeight;
+      if (isAtBottom) {
+        chatBox.scrollTop = chatBox.scrollHeight;
+      }
+    }
+  }
 
   return loading ? (
     <Box width={"100vw"} flexGrow={1} position={"relative"}>
@@ -110,11 +225,13 @@ function MedicalChat() {
           marginY={4}
           overflowY={"scroll"}
         >
-          {!(totalMessages === messages.length) && (
-            <Box ref={ref} display={"flex"} justifyContent={"center"}>
-              <PaginationLoader />
-            </Box>
-          )}
+          {
+            /*!(totalMessages === messages.length)*/ false && (
+              <Box ref={ref} display={"flex"} justifyContent={"center"}>
+                <PaginationLoader />
+              </Box>
+            )
+          }
           <Chat conversation={conversation} messages={messages} />
         </Box>
         <Flex gap={4}>
@@ -125,7 +242,7 @@ function MedicalChat() {
             value={message}
             onKeyDown={(e) => {
               if (e.key === "Enter") {
-                if (message) setMessage("");
+                if (message) sendMessage();
               }
             }}
           />
@@ -141,7 +258,7 @@ function MedicalChat() {
             alignItems={"center"}
             cursor={"pointer"}
             onClick={() => {
-              if (message) setMessage("");
+              if (message) sendMessage();
             }}
           >
             <Image src="/send.svg" alt="arrow" width={24} height={24} />
