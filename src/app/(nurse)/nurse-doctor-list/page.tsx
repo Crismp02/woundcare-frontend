75dedd53f1c3f87f6b1bcbe939143d7115174364
Cristina Morales Padron <cristimoralespadron@icloud.com>
2024-06-02 23:46:39 -0400

    List doctor

---
+++ src/app/(nurse)/nurse-doctor-list/page.tsx
@@ -0,0 +1,60 @@
+"use client"
+import Arrow from '@/components/Arrow'
+import { Box, Flex, Heading, Text } from '@chakra-ui/react'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import routes from "@/utils/routes";
+import { Doctor } from '@/interfaces/nurse/nurse.interface'
+import { getDoctors } from '@/services/nurse/nurse.service'
+import DoctorCard from '@/components/doctor-list-card/page'
+
+function doctorList() {
+    const [doctors, setDoctors] = useState<Doctor[]>();
+
+  useEffect(() => {
+   const fetchPatients = async () => {
+        const response = await getDoctors(); 
+        setDoctors(response);
+      };
+      fetchPatients();
+  }
+  , [])
+  return (
+    <Box as="main" flex={1}>
+        <Arrow />
+        <Flex
+          marginTop={-16}
+          marginRight={6}
+          flexDirection="column"
+          alignItems="flex-end"
+        >
+          <Heading
+            as="h1"
+            color="#4F1964"
+            borderBottom={"2px solid #AD8EB1"}
+            paddingX="10px"
+          >
+            Lista de especialistas 
+          </Heading>
+        </Flex>
+        <Flex
+          direction={"column"}
+          paddingX={"30px"}
+          paddingBottom={"30px"}
+          marginTop={"20px"}
+        >
+            <Flex direction={"row"} marginBottom={"10px"} justifyContent={"flex-end"}>
+          <Link href={routes.nurseRegisterDoctor}>
+          <Text color="#4F1964">+ Registrar especialista</Text>
+          </Link>
+          </Flex>
+          {doctors?.map((doctors) => (
+            <DoctorCard key={doctors.nationalId} fullName={doctors.user.fullname}/>
+          ))}
+        </Flex>
+           
+        </Box>    
+  )
+}
+
+export default doctorList
---
+++ src/components/doctor-list-card/page.tsx
@@ -0,0 +1,33 @@
+import { Box, Flex, Text } from '@chakra-ui/react'
+import Image from 'next/image'
+import React from 'react'
+import { useRouter } from "next/navigation";
+
+interface DoctorCardProps {
+    fullName: string;
+  }
+
+const DoctorCard: React.FC<DoctorCardProps> = ({ fullName }) => {
+
+  return (
+    <Flex direction={"column"}>
+          <Flex justifyContent={"space-between"}>
+          <Flex> 
+          <Box backgroundColor="#4F1964" width={"50px"} height={"50px"} borderRadius={"10px"} display={"flex"} justifyContent={"center"} alignItems={"center"}>
+          <Image
+          src="/profile/user.png"
+          alt="menu"
+          width={30}
+          height={30}
+          style={{ cursor: "pointer" }}
+        />
+          </Box>
+          <Text color="#3B3B3B" alignSelf={"center"} marginLeft={"10px"} fontWeight={"500"}>{fullName}</Text>
+          </Flex>
+          </Flex> 
+          <Box w="100%" h="2px" bg="#AD8EB1" marginBottom={3} marginTop={3}/>
+          </Flex>
+  )
+}
+
+export default DoctorCard
