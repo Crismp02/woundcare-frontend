a31b999730ea84ae86a1ee5b6e7664a53aed543b
José Vielma <josevglod@gmail.com>
2024-06-05 17:53:19 -0400

    add get nurses conversation to doctor service

--- src/services/doctor/doctor.service.ts
+++ src/services/doctor/doctor.service.ts
@@ -1,7 +1,21 @@
+import { ConversationListItem } from "@/interfaces/chat/conversation.interface";
+import { PaginatedResponse } from "@/interfaces/common/responses.interface";
 import { Doctor } from "@/interfaces/nurse/nurse.interface";
 import { fetchAPI } from "@/utils/api";
 
 export const getMe = async () => {
   const data = await fetchAPI<Doctor>("/doctor/me", "GET");
+  return data;
+};
+export const getNursesConversations = async (
+  page?: number,
+  perPage?: number
+) => {
+  const data = await fetchAPI<PaginatedResponse<ConversationListItem>>(
+    `/conversations/user/nurse?${page ? `page=${page}&` : ""}${
+      perPage ? `per-page=${perPage}` : ""
+    }`,
+    "GET"
+  );
   return data;
 };
