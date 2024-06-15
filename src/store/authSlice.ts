98220718ef75dae19411784bc4a01d3490aecfc0
José Vielma <josevglod@gmail.com>
2024-06-15 11:58:02 -0400

    fix del fix

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -31,7 +31,7 @@ export const authSlice = createSlice({
       state.authState = false;
       state.token = "";
       state.role = "";
-      localStorage.removeItem("authState");
+      localStorage.removeItem("persist:auth");
     },
   },
 });
