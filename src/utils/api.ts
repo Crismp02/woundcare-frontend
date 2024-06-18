21c4d331ef9448ba01b0f565b3ffcaa4d06d1838
José Vielma <josevglod@gmail.com>
2024-06-18 15:36:20 -0400

    add fetch files api

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -36,3 +36,36 @@ export async function fetchAPI<T>(
 
   return data as T;
 }
+
+export async function fetchFilesAPI<T>(
+  url: string,
+  method: "POST" | "PUT" | "PATCH" | "DELETE",
+  formData: FormData
+): Promise<T> {
+  const auth = localStorage.getItem("persist:auth") || "";
+  const parsedAuth = JSON.parse(auth);
+  const token = JSON.parse(parsedAuth.token);
+  const options: RequestInit = {
+    method,
+    headers: {
+      Authorization: `Bearer ${token}`,
+    },
+    body: formData,
+  };
+
+  const response = await fetch(`${BASE_URL}${url}`, options);
+
+  if (!response.ok) {
+    throw {
+      status: response.status,
+    };
+  }
+
+  if (response.status === 204) {
+    return {} as T;
+  }
+
+  const data = await response.json();
+
+  return data as T;
+}
